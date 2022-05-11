df238ed19bba843696a15fb24fc1d70b8a427c5d
Ariel <ariel.kerry@gmail.com>
2022-05-11 11:09:32 +0800

    change readme

---
+++ src/Graph.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+const client = new ApolloClient({
+  uri: "[Insert URI of GraphQL endpoint]",
+});
+
+export default class Graph extends Component {
+  render() {
+    return <div>Hello kasjdhaljshd!</div>;
+  }
+}
