41bba26279a2005f08a707969e30489bc39f0fed
Ariel <ariel.kerry@gmail.com>
2022-05-19 16:29:25 +0800

    works?

--- src/Graph.js
+++ src/Graph.js
@@ -18,13 +18,32 @@ export default class Graph extends Component {
 
   constructor(props) {
     super(props);
-    this.myRef = React.createRef();
+    this.ref = React.createRef;
+    this.ref.current.d3Force("link").distance((link) => {
+      return 30;
+    });
   }
+
+  drawNode = (node, ctx, globalScale) => {
+    ctx.textAlign = "center";
+    ctx.fillText(node.name, node.x, node.y);
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, 30, 0, 2 * Math.PI);
+    ctx.stroke();
+  };
 
   render() {
     return (
       <div>
-        <ForceGraph2D graphData={data} nodeOpacity={0.99} ref={this.myRef} />
+        <ForceGraph2D
+          graphData={data}
+          nodeOpacity={0.99}
+          linkDirectionalArrowLength={3.5}
+          linkDirectionalArrowRelPos={1}
+          ref={this.myRef}
+          onNodeHover={() => (a, b) => null}
+          nodeCanvasObject={this.drawNode}
+        />
       </div>
     );
   }
